f8dc2f3061370923c094bdfd1c2e042638bc7533
Asad <muhammadasad98980@gmail.com>
2024-02-07 03:55:13 +0500

    Completed apis for company and employee

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from api.models import *
 # Register your models here.
+
+#Display other information other than only Name
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'type')
+    search_fields=('name',)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'position')
+    search_fields=('name',)
+
+# Register our models on Admin Page
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.site_title = "CodeWithAsad"
+admin.site.site_header = "Code With Asad - Admin Panel"

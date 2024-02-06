0b838362c362e67bda802e0dfb2930149e9b3649
Asad <muhammadasad98980@gmail.com>
2024-02-07 02:13:02 +0500

    Completed Company api

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,7 @@ from api.models import Company
 # Creating Serializers              #Converting models to json
 # Creating Company Serializer
 class CompanySeriaizer(serializers.HyperlinkedModelSerializer):
+    company_id = serializers.ReadOnlyField()
     class Meta:
         model = Company
         fields = "__all__" #I want to include all the fields from Company
